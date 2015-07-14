1ab5c66284c688756f7f6bb81020a143a43b9dea
tangwz <tangwz.com@gmail.com>
2015-07-14 21:21:11 +0800

    Merge Sorted Array

---
+++ algorithm/MergeSortedArray/MergeSortedArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /*merge nums from vector's end to start,O(m+n)*/
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(j >= 0){
+            if(nums1[i] > nums2[j] && i>= 0){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
