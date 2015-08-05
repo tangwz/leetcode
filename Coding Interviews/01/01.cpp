c9866e50123ed04a246e2be47111f38306a0ff26
tangwz <tangwz.com@gmail.com>
2015-08-05 22:06:20 +0800

    跳台阶

--- Coding Interviews/01/01.cpp
+++ Coding Interviews/01/01.cpp
@@ -1,6 +1,6 @@
 class Solution {
 /*
- * 跳台阶：
+ * 跳台阶：http://www.nowcoder.com/books/coding-interviews/8c82a5b80378478f9484d87d1c5f12a4?rp=1
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  */
 public:
