62deed4e03afd70eb1c518a747d03e533ef43a4a
Tang Weizhi <tangwz.com@gmail.com>
2016-11-25 14:12:33 +0800

    add two-sum.

---
+++ leetcode/python/two-sum.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+class Solution(object):
+    """
+        Mapping the result of each element to its index.
+        Time complexity : O(n)
+        Space complexity: O(n)
+    """
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, n in enumerate(nums):
+            if n in d:
+                return [d[n], i]
+            else:
+                d[target - n] = i
