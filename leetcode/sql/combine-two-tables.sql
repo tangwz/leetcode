9c89a47739e4a54cedd58b0cb40b899f38cf5728
Tang Weizhi <tangwz.com@gmail.com>
2016-12-06 15:34:53 +0800

    add combine-two-tables

---
+++ leetcode/sql/combine-two-tables.sql
@@ -0,0 +1,12 @@
+/*
+ * This is my way and it's stupid and simple.
+ * A good and clean SQL statement I see:
+ *     SELECT `FirstName`, `LastName`, `City`, `State`
+ *     FROM `Person`
+ *     LEFT JOIN `Address`
+ *     USING(`PersonId`)
+ */
+ 
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person LEFT JOIN Address
+ON Person.PersonId = Address.PersonId;
