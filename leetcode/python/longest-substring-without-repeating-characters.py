b3a5cabb8840714f9209f69a6a16ff495c7979c3
Tang Weizhi <tangwz.com@gmail.com>
2017-04-20 16:11:56 +0800

    add longest-substring-without-repeating-characters.

---
+++ leetcode/python/longest-substring-without-repeating-characters.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+class Solution(object):
+    """ Store the word appeared in a dict.
+    """
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        been = {}
+        maxlen = start = 0
+
+        for i in range(len(s)):
+            index = been.get(s[i], None)
+            if index is not None and start <= index:
+                start = index + 1
+            else:
+                maxlen = max(maxlen, i - start + 1)
+            been[s[i]] = i
+        return maxlen
+
+
+if __name__ == '__main__':
+    test_case1 = 'abcabcbb'
+    test_case2 = 'bbbbb'
+    test_case3 = 'pwwkew'
+    test_case4 = 'dvdf'
+
+    s = Solution()
+    assert s.lengthOfLongestSubstring(test_case1) == 3
+    assert s.lengthOfLongestSubstring(test_case2) == 1
+    assert s.lengthOfLongestSubstring(test_case3) == 3
+    assert s.lengthOfLongestSubstring(test_case4) == 3
