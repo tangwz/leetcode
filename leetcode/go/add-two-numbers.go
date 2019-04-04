1d18e295e4b0b5c1c7ebfb94b4b1724c5f8e83e4
tangwz <tangwz.com@gmail.com>
2019-04-04 17:20:52 +0800

    add add-two-numbers.go

---
+++ leetcode/go/add-two-numbers.go
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=2 lang=golang
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (30.81%)
+ * Total Accepted:    810.8K
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order and each of their nodes
+ * contain a single digit. Add the two numbers and return it as a linked list.
+ * 
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	// head node
+	l := &ListNode{Val:-1, Next:nil}
+	head := l
+	carry := 0
+	for l1 != nil || l2 != nil || carry != 0 {
+		if l1 != nil {
+			carry += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			carry += l2.Val
+			l2 = l2.Next
+		}
+
+		head.Next = &ListNode{Val: carry % 10, Next: nil}
+
+		carry = carry / 10
+		head = head.Next
+	}
+	return l.Next
+}
+
